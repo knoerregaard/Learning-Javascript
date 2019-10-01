8d27199c332cf77d11b6781be379d3096b5db3bf
Klaus <knoerregaard@gmail.com>
2019-10-01 16:31:22 +0200

    update

---
+++ pass.js
@@ -0,0 +1,10 @@
+module.exports = [
+    {
+        url : "https://www.flyttilskive.dk/umbraco/surface/Search/GetActivity",
+        body: {"type":"natur","id":"map_1c91f0b8-3c0b-4d94-940b-7b2b9c32df3a","tags":"running","customlist":0}
+    },
+    {
+        url : ""
+
+    }
+]
