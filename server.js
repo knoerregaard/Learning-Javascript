9e75c2903af4780d981ad547fdc1d8de9faac7ad
Klaus <knoerregaard@gmail.com>
2019-10-01 16:36:45 +0200

    update

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+
 app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: 'World'}));
 });
+app.use(express.static('public'))
+app.use(express.static('xmlhttprequest'))
+
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
 });
