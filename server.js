75c69b789c507790e6227d3eabd3a30227b08b17
Klaus <knoerregaard@gmail.com>
2019-10-01 16:40:47 +0200

    update

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
 
+app.use('/xmlhttprequest', express.static(path.join(__dirname, 'public')))
+
 app.get('/', function (req, res) {
  res.send(JSON.stringify({ Hello: 'World'}));
 });
 
-app.use('/xmlhttprequest', express.static(path.join(__dirname, 'public')))
 
 app.listen(port, function () {
  console.log(`Example app listening on port !`);
