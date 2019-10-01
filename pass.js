1dff8b5bd5231c9a522ac0a0911966b5293b0330
Klaus <knoerregaard@gmail.com>
2019-10-01 22:25:59 +0200

    ipdate

--- pass.js
+++ pass.js
@@ -1,10 +1,13 @@
-module.exports = [
-    {
-        url : "https://www.flyttilskive.dk/umbraco/surface/Search/GetActivity",
-        body: {"type":"natur","id":"map_1c91f0b8-3c0b-4d94-940b-7b2b9c32df3a","tags":"running","customlist":0}
-    },
-    {
-        url : ""
-
-    }
-]
+export default
+{
+    
+    "flyttilskive" :   {
+            url : "https://www.flyttilskive.dk/umbraco/surface/Search/GetActivity",
+            body: {"type":"natur","id":"map_1c91f0b8-3c0b-4d94-940b-7b2b9c32df3a","tags":"running","customlist":0}
+        },
+    "pindle" :{
+            url : "https://admin.pindle.dk/api/v2/events?language=da&timestamp=0",
+            token : "276157c242ced74fe2b33c0c9e1414fac0fb0a5afcce6908f2681fd34dff060e"
+        }
+    
+}
